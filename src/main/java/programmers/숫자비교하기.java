276b4564e170f27ed8c6f1ad4ad4b2d5b85d69b6
인성준 <tjdwns1081@gmail.com>
2025-02-28 22:10:56 +0900

    practice chap01

---
+++ src/main/java/programmers/숫자비교하기.java
@@ -0,0 +1,25 @@
+package programmers;
+
+public class 숫자비교하기 {
+
+ public static void main(String[] args) {
+     /* 문제 설명
+      * 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+
+      * 제한사항
+      * 0 ≤ num1 ≤ 10,000
+      * 0 ≤ num2 ≤ 10,000
+      */
+
+     int num1 = 2;
+     int num2 = 2;
+
+    if (num1 == num2) {
+        System.out.println("1");
+    }
+    else {
+        System.out.println("-1");
+    }
+ }
+}
+
