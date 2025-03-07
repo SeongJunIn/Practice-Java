12c42f9f1d9c60c496a6221bb9f170fb0e2a802c
인성준 <tjdwns1081@gmail.com>
2025-03-07 09:06:22 +0900

    practice Java

--- src/main/java/groom/chap04/section01/conditional/Application.java
+++ src/main/java/groom/chap04/section01/conditional/Application.java
@@ -9,25 +9,25 @@ public class Application {
 //        a.testSimpleIfStatement();
 
         /* 중첩 if문 흐름 확인용 메소드 호출 */
-        a.testNestedIfStatement();
+//        a.testNestedIfStatement();
 
         B_ifElse b = new B_ifElse();
         /* 단독 if-else 문 흐름 확인용 메소드 호출 */
-        //b.testSimpleIfElseStatement();
+//        b.testSimpleIfElseStatement();
 
         /* 중첩 if-else 문 흐름 확인용 메소드 호출 */
-        //b.testNestedIfElseStatement();
+//        b.testNestedIfElseStatement();
 
         C_ifElseIf c = new C_ifElseIf();
         /* 단독 if-else 문 흐름 확인용 메소드 호출 */
-        //c.testSimpleIfElseIfStatement();
+//        c.testSimpleIfElseIfStatement();
 
         /* 중첩 if-else-if 문 흐름 확인용 메소드 호출 */
-        //c.testNestedIfElseIfStatement();
+//        c.testNestedIfElseIfStatement();
 
         D_switch d = new D_switch();
         /* switch문 흐름 확인용 메소드 호출 */
-        //d.testSimpleSwitchStatement();
+        d.testSimpleSwitchStatement();
 
         /* 문자열값 비교 및 break 테스트 */
 //        d.testSwitchVendingMachine();
--- src/main/java/groom/chap04/section01/conditional/D_switch.java
+++ src/main/java/groom/chap04/section01/conditional/D_switch.java
@@ -1,4 +1,56 @@
 package groom.chap04.section01.conditional;
+import java.util.Scanner;
 
 public class D_switch {
+
+    public void testSimpleSwitchStatement(){
+
+        /* 목표. switch문 단독 사용에 대한 흐름을 이해가 적용할 수 있다. */
+
+        /* 정수 두 개와 연산 기호 문자를 입력 받아서
+         * 두 숫자의 연산 결과를 출력해보는 간단한 계산기 만들기
+         */
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("첫 번째 정수 입력 : ");
+        int first = sc.nextInt();
+        System.out.println("두 번째 정수 입력 : ");
+        int second = sc.nextInt();
+        System.out.println("연산 기호 입력(+, -, *, /, %) : ");
+        char op = sc.next().charAt(0);
+
+        /* 연산의 결과를 저장할 변수 */
+        int result = 0;
+
+        /* 입력한 연산기호 문자에 따라 연간 결과가 달라짐 */
+        switch(op){
+
+            case '+':
+                result = first + second;
+                break;
+
+            case '-':
+                result = first - second;
+                break;
+
+                case '*':
+                    result = first * second;
+                    break;
+
+                    case '/':
+                        result = first / second;                    //second가 0이면 에러 발생
+                        break;
+
+            case  '%':
+                result = first % second;
+                break;
+
+            /* default문은 생략 가능하지만 생략하는 경우 주석으로 생략 이유를 작성해주는 것이 좋다. */
+            /* 산술연산 이외 다른 문자에 대한 처리는 생략하였음 */
+
+        }
+
+        System.out.println(first + " " + op + second + " " + " = " + result);
+
+    }
 }
